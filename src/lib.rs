2518e99a408f6b22bb9e6b2a0fca5314cbb8580d
gregoryalary <gregalary26@gmail.com>
2018-10-20 19:41:40 +0200

    refactor: created the wrapping module

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,117 @@
-pub struct BinaryHeap {
-    heap: Vec<i32>
-}
+pub mod binaryheap {
 
-impl BinaryHeap {
-
-    pub fn count(&self) -> usize {
-        return self.heap.len();
+    pub struct BinaryHeap {
+        heap: Vec<i32>
     }
 
-    pub fn add(&mut self, key : i32) {
-        self.heap.push(key);
-        let last = self.heap.len() - 1;
-        self.percolate_up(last);
-    }
+    impl BinaryHeap {
 
-    pub fn remove(&mut self) -> Option<i32> {
-        match self.heap.pop() {
-            None    => None,
-            Some(x) => {
-                return if !self.heap.is_empty() {
-                    let temp = self.heap[0];
-                    self.heap[0] = x;
-                    self.percolate_down(0);
-                    Some(temp)
-                } else {
-                    Some(x)
-                }
-            } 
-        }
-    }
-
-    pub fn build_heap(&mut self) {
-        for i in (0..(self.heap.len() / 2)).rev() {
-            self.percolate_down(i);
-        }
-    }
-
-    fn left(&self, i : usize) -> usize {
-        return (i * 2) + 1;
-    }
-
-    fn right(&self, i : usize) -> usize {
-        return self.left(i) + 1;
-    }
-
-    fn parent(&self, i : usize) -> Option<usize> {
-        return if i > 0 {
-            Some((i - 1) / 2)
-        } else {
-            None
-        }
-    }
-
-    fn percolate_down(&mut self, index : usize) {
-
-        let left = self.left(index);
-        let right = self.right(index);
-        let mut m = if left < self.heap.len() && self.heap[index] < self.heap[left] { left } else { index };
-        m = if right < self.heap.len() && self.heap[m] < self.heap[right] { right } else { m };
-
-        if m != index {
-            let temp = self.heap[index];
-            self.heap[index] = self.heap[m];
-            self.heap[m] = temp;
-            self.percolate_down(m);
+        /**
+         * Return the number of element in the heap
+         */
+        pub fn count(&self) -> usize {
+            return self.heap.len();
         }
 
-    }
+        /**
+         * Add a new element into the heap
+         */
+        pub fn add(&mut self, key : i32) {
+            self.heap.push(key);
+            let last = self.heap.len() - 1;
+            self.percolate_up(last);
+        }
 
-    fn percolate_up(&mut self, index : usize) {
-        let opt_parent = self.parent(index);
-        if let Some(parent) = opt_parent {
-            if index > 0 && self.heap[parent] < self.heap[index] {
-                let temp = self.heap[parent];
-                self.heap[parent] = self.heap[index];
-                self.heap[index] = temp;
-                self.percolate_up(parent);
+        /**
+         * Remove the greatest element of the heap
+         * Returns None if the heap is empty, or Some(x) otherwise
+         */
+        pub fn remove(&mut self) -> Option<i32> {
+            match self.heap.pop() {
+                None    => None,
+                Some(x) => {
+                    return if !self.heap.is_empty() {
+                        let temp = self.heap[0];
+                        self.heap[0] = x;
+                        self.percolate_down(0);
+                        Some(temp)
+                    } else {
+                        Some(x)
+                    }
+                } 
             }
         }
+
+        fn left(&self, i : usize) -> usize {
+            return (i * 2) + 1;
+        }
+
+        fn right(&self, i : usize) -> usize {
+            return self.left(i) + 1;
+        }
+
+        fn parent(&self, i : usize) -> Option<usize> {
+            return if i > 0 {
+                Some((i - 1) / 2)
+            } else {
+                None
+            }
+        }
+
+        fn percolate_down(&mut self, index : usize) {
+
+            let left = self.left(index);
+            let right = self.right(index);
+            let mut m = if left < self.heap.len() && self.heap[index] < self.heap[left] { left } else { index };
+            m = if right < self.heap.len() && self.heap[m] < self.heap[right] { right } else { m };
+
+            if m != index {
+                let temp = self.heap[index];
+                self.heap[index] = self.heap[m];
+                self.heap[m] = temp;
+                self.percolate_down(m);
+            }
+
+        }
+
+        fn percolate_up(&mut self, index : usize) {
+            let opt_parent = self.parent(index);
+            if let Some(parent) = opt_parent {
+                if index > 0 && self.heap[parent] < self.heap[index] {
+                    let temp = self.heap[parent];
+                    self.heap[parent] = self.heap[index];
+                    self.heap[index] = temp;
+                    self.percolate_up(parent);
+                }
+            }
+        }
+
+        pub fn build_heap(&mut self) {
+            for i in (0..(self.heap.len() / 2)).rev() {
+                self.percolate_down(i);
+            }
+        }
+
     }
 
-}
+    /**
+     * Returns a new binary heap built from the key
+     * passed to the function
+     */
+    pub fn build_heap(keys : &[i32]) -> BinaryHeap {
+        let mut b_heap = BinaryHeap {
+            heap : keys.to_vec()
+        };
+        b_heap.build_heap();
+        return b_heap;
+    }
 
-pub fn build_heap(keys : &[i32]) -> BinaryHeap {
-    let mut b_heap = BinaryHeap {
-        heap : keys.to_vec()
-    };
-    b_heap.build_heap();
-    return b_heap;
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::*;
+    use super::binaryheap::*;
 
     #[test]
     fn test_build_heap() {
@@ -106,7 +124,6 @@ mod tests {
         assert_eq!(heap.count(), 0);
         // Test 2
         let mut heap = build_heap(&[3, -1, 0, 10, 6, 4, 5]);
-        println!("-> {:?}", heap.heap);
         assert_eq!(heap.remove(), Some(10));
         assert_eq!(heap.remove(), Some(6));
         assert_eq!(heap.remove(), Some(5));
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate binaryheap;
+use binaryheap::binaryheap::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut heap = build_heap(&[-1, 6, 9, 0]);
+    heap.add(3);
+    heap.add(11);
+    while heap.count() > 0 {
+        if let Some(x) = heap.remove() {
+            println!("{}", x);
+        }
+    } // will print 11, 9, 6, 3, 0, -1
 }
